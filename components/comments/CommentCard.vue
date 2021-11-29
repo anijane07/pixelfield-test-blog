a5ae775ee721a3497d09a5dfbcfbef52bde05ffa
Jana Schořová <schorjan@fit.cvut.cz>
2021-11-29 09:38:32 +0100

    Add comment component

---
+++ components/comments/CommentCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <article>
+    <div class="comment-avatar">
+      <v-avatar size="62">
+        <v-img :src="user.avatar"></v-img>
+      </v-avatar>
+    </div>
+    <div class="comment-person">
+      <h4>{{ fullname }}</h4>
+      <span>{{ user.email }}</span
+      ><br />
+      <span>{{ created }}</span>
+    </div>
+    <div class="comment-content">
+      <p>{{ text }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['user', 'created', 'text'],
+  computed: {
+    fullname() {
+      return `${this.user.name} ${this.user.surname}`
+    },
+  },
+}
+</script>
+
+<style scoped>
+article {
+  margin: 1rem;
+  padding: 1rem;
+  background-color: aliceblue;
+  display: flex;
+}
+
+.comment-person {
+  display: block;
+}
+</style>
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -2,15 +2,32 @@
   <section>
     <v-card>
       <PostDetail />
+      <h3>Comments</h3>
+      <CommentCard :user="user" :created="created_on" :text="text" />
     </v-card>
   </section>
 </template>
 
 <script>
 import PostDetail from '~/components/posts/PostDetail.vue'
+import CommentCard from '~/components/comments/CommentCard.vue'
+
 export default {
   components: {
     PostDetail,
+    CommentCard,
+  },
+  data() {
+    return {
+      user: {
+        avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+        name: 'John',
+        surname: 'Trem',
+        email: 'j.trem@email.com',
+      },
+      created_on: new Date(2018 - 11 - 21),
+      text: 'Nice post, I would like to get other post by you.',
+    }
   },
 }
 </script>
